36845ae54267ad7fda213ec0bcbf33f53fb4e9ce
EmelineH <emeline.hourmand@gmail.com>
2019-06-25 14:16:52 +0200

    Update entity Add state DAO

--- src/fr/eni/sortircom/dal/dao/DAOFactory.java
+++ src/fr/eni/sortircom/dal/dao/DAOFactory.java
@@ -1,9 +1,6 @@
 package fr.eni.sortircom.dal.dao;
 
-import fr.eni.sortircom.dal.dao.hibernate.HibernateCityDAO;
-import fr.eni.sortircom.dal.dao.hibernate.HibernateEventDAO;
-import fr.eni.sortircom.dal.dao.hibernate.HibernateParticipantDAO;
-import fr.eni.sortircom.dal.dao.hibernate.HibernatePlaceDAO;
+import fr.eni.sortircom.dal.dao.hibernate.*;
 
 /**
  *
@@ -19,5 +16,9 @@ public class DAOFactory {
     public static CityDAO getCityDAO() { return new HibernateCityDAO(); }
 
     public static PlaceDAO getPlaceDAO() { return new HibernatePlaceDAO(); }
+
+    public static SiteDAO getSiteDAO() { return new HibernateSiteDAO(); }
+
+    public static StateDAO getStateDAO() { return new HibernateStateDAO(); }
 
 }
---
+++ src/fr/eni/sortircom/dal/dao/hibernate/HibernateStateDAO.java
@@ -0,0 +1,61 @@
+package fr.eni.sortircom.dal.dao.hibernate;
+
+import fr.eni.sortircom.bo.Event;
+import fr.eni.sortircom.bo.Site;
+import fr.eni.sortircom.bo.State;
+import fr.eni.sortircom.dal.ConnectionProvider;
+import fr.eni.sortircom.dal.dao.StateDAO;
+import fr.eni.sortircom.dal.exception.DALException;
+import org.hibernate.Session;
+
+import javax.persistence.Query;
+import java.util.List;
+
+/**
+ * @author Emeline Hourmand
+ */
+public class HibernateStateDAO implements StateDAO {
+
+    @Override
+    public void insert(State state) throws DALException {
+        Session session = ConnectionProvider.getConnection();
+        session.beginTransaction();
+        session.save(state);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public List<State> selectAll() throws DALException {
+        Session session = ConnectionProvider.getConnection();
+        Query q = session.createQuery("FROM Event");
+        List<State> stateList = q.getResultList();
+        return stateList;
+    }
+
+    @Override
+    public State selectById(Long id) throws DALException {
+        Session session = ConnectionProvider.getConnection();
+        Query q = session.createQuery("FROM State WHERE id=:idState");
+        q.setParameter("idState", id);
+        List<State> states = q.getResultList();
+        State state = states.get(0);
+        return state;
+    }
+
+    @Override
+    public void update(State state) throws DALException {
+        Session session = ConnectionProvider.getConnection();
+        session.beginTransaction();
+        session.saveOrUpdate(state);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public void delete(Long id) throws DALException {
+        Session session = ConnectionProvider.getConnection();
+        State state = (State) session.get(State.class, id);
+        session.beginTransaction();
+        session.delete(state);
+        session.getTransaction().commit();
+    }
+}
