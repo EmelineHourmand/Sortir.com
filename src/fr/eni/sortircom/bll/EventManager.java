fe70a927a00c58e156b75b4d6e43dc2d1ff705cc
EmelineH <emeline.hourmand@gmail.com>
2019-06-26 15:54:10 +0200

    update manager event

--- src/fr/eni/sortircom/bll/EventManager.java
+++ src/fr/eni/sortircom/bll/EventManager.java
@@ -1,7 +1,6 @@
 package fr.eni.sortircom.bll;
 
 import fr.eni.sortircom.bll.exception.BLLException;
-import fr.eni.sortircom.bll.exception.CodesErreursBLL;
 import fr.eni.sortircom.bo.Event;
 import fr.eni.sortircom.dal.dao.DAOFactory;
 import fr.eni.sortircom.dal.dao.EventDAO;
@@ -17,7 +16,7 @@ public abstract class EventManager {
     private static EventDAO eventDAO;
 
     /**
-     * Constructor
+     * Constructor (Singleton)
      * @throws BLLException
      */
     public EventManager() {
@@ -46,99 +45,44 @@ public abstract class EventManager {
      *
      * @throws BLLException
      */
-   /* public static void checkEvent(Event event, BLLException bllException) throws BLLException {
-
-        boolean isOK = true;
-
-        // Verifie si l'event est pas pas null
-        if(event == null){
-            bllException.addErreur(CodesErreursBLL.EVENT_NULL_ERROR);
-        } else {
-
-            // Verification du nom de l'event
-            if(event.getName() == null || event.getName().trim().length()==0){
-                sb.append("Le nom est obligatoire.\n");
-                isOK = false;
-            }
-            if(event.getName().length() > 255) {
-                sb.append("Le nom doit avoir moins de 256 caractères.\n");
-                isOK = false;
-            }
-
-            // Verification de la description de l'event
-            if(event.getDescription()== null || event.getDescription().trim().length()==0){
-                sb.append("La description est obligatoire.\n");
-                isOK = false;
-            }
-
-
-            if(user.getLastName()==null || user.getLastName().trim().length()==0){
-                sb.append("Le nom de famille est obligatoire.\n");
-                isOK = false;
-            }
-            if (user.getEmail() == null || user.getEmail().trim().length()== 0) {
-                sb.append("L'email est obligatoire.");
-                isOK = false;
-            } else {
-                boolean emailCheck = emailCheck(user.getEmail());
-                if (emailCheck == false) {
-                    sb.append("L'email n'est pas valide.");
-                    isOK = false;
-                }
-            }
-            if (user.getPhoneNumber() != null || user.getPhoneNumber().trim().length() > 0) {
-                boolean numberCheck = numberCheck(user.getPhoneNumber());
-                if (numberCheck == false) {
-                    sb.append("Le numéro de téléphone n'est pas valide");
-                    isOK = false;
-                }
-            }
-            if (user.getStreet() == null || user.getStreet().trim().length() ==0) {
-                sb.append("La rue n'est pas valide.");
-                isOK = false;
-            }
-            if (user.getPostalCode() == null || user.getPostalCode().trim().length() == 0) {
-                sb.append("Le code postal est obligatoire");
-                isOK = false;
-            } else {
-                if (user.getPostalCode().length() > 5 && user.getPostalCode().length() < 5) {
-                    sb.append("Le code postal est invalide");
-                    isOK = false;
-                }
-            }
-            if (user.getCity() == null || user.getCity().trim().length() ==0 ) {
-                sb.append("La ville postal est obligatoire");
-                isOK = false;
-            }
-            if (user.getPassword() == null || user.getPassword().trim().length() ==0 ) {
-                sb.append("Le password est obligatoire");
-                isOK = false;
-            }
-
-            if(!isOK) {
-                throw new BLLException(sb.toString());
-            }
-        }
+    public static boolean checkEvent(Event event, BLLException bllException) throws BLLException {
+        return false;
     }
 
     public List<Event> selectAllEvent() throws BLLException {
-        return eventDAO.selectAll();
+        try {
+            return eventDAO.selectAll();
+        } catch (DALException e) {
+            BLLException bllException = new BLLException();
+            bllException.addSuppressed(e);
+            throw bllException;
+        }
     }
 
     public Event selectEvent(Long id) throws BLLException {
-        return eventDAO.selectById(id);
+        try {
+            return eventDAO.selectById(id);
+        }catch (DALException e) {
+            BLLException bllException = new BLLException();
+            bllException.addSuppressed(e);
+            throw bllException;
+        }
     }
 
     public void updateEvent(Event event) throws  BLLException {
-        eventDAO.update(event);
+        try {
+            eventDAO.update(event);
+        } catch (DALException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeEvent(Long id) throws BLLException {
-        eventDAO.delete(id);
+        try {
+            eventDAO.delete(id);
+        } catch (DALException e) {
+            e.printStackTrace();
+        }
     }
-*/
+
 }
-
-
-
-
